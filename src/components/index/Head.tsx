dac7ad1c595dcf2a5d1b6df5d14b8aace5d1fdd4
Gibsol <gibsol75@gmail.com>
2023-07-24 16:23:24 +0300

    Implement Head component
    This component has a header and description.

---
+++ src/components/index/Head.tsx
@@ -0,0 +1,10 @@
+export const Head = () => {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold ">Paul Bogatyr</h1>
+      <p className="text-sm italic">
+        Crafting Digital Dreams: Fullstack Web Wizardry
+      </p>
+    </section>
+  );
+};
