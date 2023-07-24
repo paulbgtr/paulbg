a023c1a147f2de5209444843c6367eec38b0e26d
Gibsol <gibsol75@gmail.com>
2023-07-24 16:24:15 +0300

    Passion component
    This component includes a text that speaks about my passion and hobbies.

---
+++ src/components/index/Passion.tsx
@@ -0,0 +1,15 @@
+export const Passion = () => {
+  return (
+    <section>
+      <h2 className="text-xl font-bold">Passion</h2>
+      <p>
+        When not coding, you will find me lost in the world of music, playing
+        drums, bass, and guitar while managing my own musical project. Fitness
+        is my sanctuary, as I embrace going to gym, long-distance running and
+        calisthenics. I cherish moments with loved ones, constantly seek
+        knowledge through reading, and believe in the power of continuous
+        growth.
+      </p>
+    </section>
+  );
+};
