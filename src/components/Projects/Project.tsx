2425a6cd2be00602e92624e7f24b927d8f69eb23
Gibsol <gibsol75@gmail.com>
2023-07-24 16:25:41 +0300

    Project component
    This is a daisyUI card wrapped in Link nextjs component.

---
+++ src/components/Projects/Project.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export const Project = ({
+  title,
+  description,
+  link,
+}: {
+  title: string;
+  description: string;
+  link: string;
+}) => {
+  return (
+    <Link className="duration-200 hover:opacity-90" href={link}>
+      <div className="h-full shadow-xl card bg-base-100 image-full">
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
