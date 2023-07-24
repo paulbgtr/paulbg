a865f0a249bf2489fff34dc0793bb4fd185b2c3a
Gibsol <gibsol75@gmail.com>
2023-07-24 16:33:07 +0300

    Layout conditional rendering for 404
    Remove additional styling of the Layout component on the /404 page.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,25 @@
 import { ReactNode } from "react";
+import { useRouter } from "next/router";
 
 import { Navbar } from "./Navbar";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
+  const router = useRouter();
+
   return (
     <>
-      <Navbar />
-      <div className="px-4 py-2 md:py-4 lg:py-6 xl:py-8 md:px-8 lg:px-12 xl:px-16">
-        <div className="max-w-3xl mx-auto">
-          <div className="grid gap-3">{children}</div>
-        </div>
-      </div>
+      {router.pathname === "/404" ? (
+        <div>{children}</div>
+      ) : (
+        <>
+          <Navbar />
+          <div className="px-4 py-2 md:py-4 lg:py-6 xl:py-8 md:px-8 lg:px-12 xl:px-16">
+            <div className="max-w-3xl mx-auto">
+              <div className="grid gap-3">{children}</div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
