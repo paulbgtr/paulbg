6f9f36054f9341d100b5902ea76ba1fe946a8f95
Gibsol <gibsol75@gmail.com>
2023-07-24 16:21:48 +0300

    Implement Navbar component
    This is a simple daisyui component. It also has a custom mx.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <div className="mx-auto max-w-7xl navbar bg-base-100">
+      <div className="flex-1">
+        <Link href="/" className="text-xl normal-case btn btn-ghost">
+          Paul Bogatyr
+        </Link>
+      </div>
+      <div className="flex-none">
+        <ul className="px-1 menu menu-horizontal">
+          <li>
+            <Link href="https://medium.com/@gibsol75">Posts</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
