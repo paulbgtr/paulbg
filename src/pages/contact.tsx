547d1089bf453a32d3da733a7b75f74f654fb6bb
Gibsol <gibsol75@gmail.com>
2023-07-25 13:04:36 +0300

    Contact page implementation

---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/contact/Header";
+import { ContactForm } from "@/components/contact/ContactForm";
+
+const Contact = () => {
+  return (
+    <div className="justify-self-center w-96">
+      <Header />
+      <ContactForm />
+    </div>
+  );
+};
+
+export default Contact;
