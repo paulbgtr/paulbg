b744e378beffdb478a251d57643a8f625e68553c
Gibsol <gibsol75@gmail.com>
2023-07-25 13:03:57 +0300

    Implement Header for contact page

---
+++ src/components/contact/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold">Contact</h1>
+      <p className="text-sm italic">Contact me via email</p>
+    </section>
+  );
+};
