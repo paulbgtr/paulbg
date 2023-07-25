7f14267a8a9aa606c5696b2b8f0d4f0bd3b4a01a
Gibsol <gibsol75@gmail.com>
2023-07-25 13:11:46 +0300

    Add head to contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,12 +1,24 @@
+import Head from "next/head";
+
 import { Header } from "@/components/contact/Header";
 import { ContactForm } from "@/components/contact/ContactForm";
 
 const Contact = () => {
   return (
-    <div className="justify-self-center w-96">
-      <Header />
-      <ContactForm />
-    </div>
+    <>
+      <Head>
+        <title>Paul Bogatyr / Contact</title>
+        <meta
+          name="description"
+          content="Crafting Digital Dreams: Fullstack Web Wizardry"
+        />
+      </Head>
+
+      <div className="justify-self-center w-96">
+        <Header />
+        <ContactForm />
+      </div>
+    </>
   );
 };
 
