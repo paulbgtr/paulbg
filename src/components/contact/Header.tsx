446fe6970779086372cda00c6129e4a111175776
Gibsol <gibsol75@gmail.com>
2023-08-04 09:16:44 +0300

    Remove old Header components

--- src/components/contact/Header.tsx
+++
@@ -1,8 +0,0 @@
-export const Header = () => {
-  return (
-    <section>
-      <h1 className="text-3xl font-bold">Contact</h1>
-      <p className="text-sm italic">Contact me via email</p>
-    </section>
-  );
-};
