987182b2076e7ddf035131e821dd784995d81a39
paulbg <paulbg@hey.com>
2023-10-22 09:58:28 +0300

    feat(Header.tsx): add support for displaying an avatar image in the header section
    The changes in this commit add support for displaying an avatar image in the header section of the application.
    
    - A new image file `pfp.jpeg` is added to the `public` directory.
    - The `Header` component now accepts an optional `image` prop.
    - If the `image` prop is provided and truthy, an avatar image is displayed in the header section.
    - The avatar image is sourced from the `pfp.jpeg` file.
    - The avatar image is wrapped in a div with appropriate styling.
    
    This change enhances the visual appeal of the header section and allows for personalization by displaying an avatar image if desired.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,32 @@
 import { ReactNode } from "react";
+import Image from "next/image";
+import pfp from "/public/pfp.jpeg";
 
 export const Header = ({
   title,
   description,
+  image,
   children,
 }: {
   title: string;
   description: string;
+  image?: boolean;
   children?: ReactNode;
 }) => {
   return (
-    <section>
-      <h1 className="text-3xl font-bold">{title}</h1>
-      <p className="text-sm italic">{description}</p>
-      {children}
+    <section className="flex gap-8">
+      {image && (
+        <div className="avatar">
+          <div className="w-24 h-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+            <Image src={pfp} alt={title} />
+          </div>
+        </div>
+      )}
+      <div>
+        <h1 className="text-3xl font-bold">{title}</h1>
+        <p className="text-sm italic">{description}</p>
+        {children}
+      </div>
     </section>
   );
 };
