7c4178c849ed698a569949671288aa7ed65d2fd4
paulbg <paulbg@hey.com>
2024-07-25 13:49:30 +0300

    chore: Project - thicker border

--- src/components/pageRelated/index/Projects/Project.tsx
+++ src/components/pageRelated/index/Projects/Project.tsx
@@ -11,7 +11,7 @@ export const Project = ({
 }) => {
   return (
     <Link href={link}>
-      <div className="h-full duration-200 border-2 shadow-md hover:text-white hover:bg-primary border-primary card bg-base-100">
+      <div className="h-full duration-200 border-4 shadow-md hover:text-base-100 hover:bg-primary border-primary card bg-base-100">
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
           <p>{description}</p>
