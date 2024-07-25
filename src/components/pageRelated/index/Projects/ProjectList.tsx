cca0bd6efa03cdb1989254b14b284ab5178ecd5c
paulbg <paulbg@hey.com>
2024-07-25 13:21:22 +0300

    chore: add emojis and remove unnecessary html

--- src/components/pageRelated/index/Projects/ProjectList.tsx
+++ src/components/pageRelated/index/Projects/ProjectList.tsx
@@ -4,39 +4,36 @@ export const ProjectList = () => {
   const projectData = [
     {
       id: 1,
-      title: "nottoto",
+      title: "📔 nottoto",
       description:
         "A fullstack CLI frontend + backend application for managing notes.",
       link: "https://github.com/paulbgtr/nottoto",
     },
     {
       id: 2,
-      title: "recipify",
+      title: "🥗 recipify",
       description:
         "A fullstack web app for finding recipes based on ingredients you have.",
       link: "https://github.com/paulbgtr/recipify",
     },
     {
       id: 3,
-      title: "pomodoro cli",
+      title: "🍅 pomodoro cli",
       description: "A CLI tool for managing your work sessions.",
       link: "http://github.com/paulbgtr/pomodoro-cli",
     },
   ];
 
   return (
-    <ul>
-      <div className="grid grid-cols-1 gap-3 md:grid-cols-3">
-        {projectData.map((project) => (
-          <li key={project.id}>
-            <Project
-              title={project.title}
-              description={project.description}
-              link={project.link}
-            />
-          </li>
-        ))}
-      </div>
-    </ul>
+    <div className="grid grid-cols-1 gap-3 md:grid-cols-3">
+      {projectData.map((project) => (
+        <Project
+          key={project.id}
+          title={project.title}
+          description={project.description}
+          link={project.link}
+        />
+      ))}
+    </div>
   );
 };
