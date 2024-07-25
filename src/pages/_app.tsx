41b25bcdaeb6049b8d0944f7ba2cc4d28e2aff8f
paulbg <paulbg@hey.com>
2024-07-25 13:21:47 +0300

    chore: remove app opacity animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,12 @@
-import '@/styles/tailwind.css'
-import type { AppProps } from 'next/app'
-import { useState, useEffect } from 'react'
+import "@/styles/tailwind.css";
+import type { AppProps } from "next/app";
 
-import { Layout } from '@/components/Layout'
+import { Layout } from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [isShown, setIsShown] = useState(false)
-
-  useEffect(()=>{
-    setIsShown(true)
-  }, [])
-
   return (
-    <div className={`${isShown ? "opacity-100" : "opacity-0"} duration-1000`}>
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    </div>
-  )
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
