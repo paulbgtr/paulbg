27857a7e2ddfa43942ecc925e52773bad0bacd4e
paulbg <paulbg@hey.com>
2024-07-25 13:20:37 +0300

    feat: impl BIO component

---
+++ src/components/pageRelated/index/Bio.tsx
@@ -0,0 +1,17 @@
+import { Heading } from "@/components/Heading";
+
+export const Bio = () => {
+  return (
+    <section className="space-y-4">
+      <Heading text="Bio" />
+      <p>
+        <span className="pr-1 font-bold">2021 - Present:</span> Studying
+        Software Engineering at Tallinna Polütehnikum
+      </p>
+      <p>
+        <span className="pr-1 font-bold">2024 - Present:</span> Fullstack
+        Software Engineer Intern at Z-Bit
+      </p>
+    </section>
+  );
+};
