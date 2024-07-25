d5b159326fb2ac6ff4d58c662a04634a66c48e33
paulbg <paulbg@hey.com>
2024-07-25 13:48:55 +0300

    chore: clean Layout and add px to mobile

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,25 +1,18 @@
 import { ReactNode } from "react";
-import { useRouter } from "next/router";
 
 import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
-  const router = useRouter();
-
   return (
     <>
-      {router.pathname === "/404" ? (
-        <div>{children}</div>
-      ) : (
-        <>
-          <Navbar />
-          <div className="px-4 py-2 md:py-4 lg:py-6 xl:py-8 md:px-8 lg:px-12 xl:px-16">
-            <div className="max-w-3xl mx-auto">
-              <div className="grid gap-3">{children}</div>
-            </div>
-          </div>
-        </>
-      )}
+      <article className="min-h-screen px-4 md:px-0">
+        <Navbar />
+        <div className="max-w-3xl mx-auto">
+          <div className="grid gap-3">{children}</div>
+        </div>
+      </article>
+      <Footer />
     </>
   );
 };
