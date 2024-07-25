b99ab4ef7559d4d3e4a1ad3e46d05a9331f85063
paulbg <paulbg@hey.com>
2024-07-25 13:49:17 +0300

    chore: add note on more projects to ProjectList

--- src/components/pageRelated/index/Projects.tsx
+++ src/components/pageRelated/index/Projects.tsx
@@ -1,3 +1,4 @@
+import { PrettyLink } from "./PrettyLink";
 import { ProjectList } from "./Projects/ProjectList";
 import { Heading } from "@/components/Heading";
 
@@ -6,6 +7,13 @@ export const Projects = () => {
     <section className="flex flex-col gap-2 space-y-4">
       <Heading text="Projects" />
       <ProjectList />
+      <p className="italic">
+        P.S. More cool projects on my{" "}
+        <PrettyLink href="https://github.com/paulbgtr?tab=repositories">
+          GitHub
+        </PrettyLink>
+        . 🌟
+      </p>
     </section>
   );
 };
