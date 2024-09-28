144f20b8ebcafe7c45f1ade8f7b433cfe314e7b7
paulbg <paulbg@hey.com>
2024-09-28 14:34:11 +0300

    chore: remove avatar from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,15 +15,15 @@ export const Header = ({
 }) => {
   return (
     <section className="flex gap-8">
-      {image && (
+      {/* {image && (
         <div className="avatar">
           <div className="w-24 h-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
             <Image src={pfp} alt={title} />
           </div>
         </div>
-      )}
-      <div>
-        <h1 className="text-3xl font-bold">{title}</h1>
+      )} */}
+      <div className="space-y-1">
+        <h1 className="text-4xl font-black">{title}</h1>
         <p className="text-sm italic">{description}</p>
         {children}
       </div>
