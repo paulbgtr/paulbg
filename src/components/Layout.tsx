9157649d57a8562d45c93458dc5798394fa8952f
paulbg <paulbg@hey.com>
2024-10-30 16:13:05 +0200

    chore: remove footer and navbar from layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,16 @@
 import { ReactNode } from "react";
 
 import { Navbar } from "./Navbar";
-import { Footer } from "./Footer";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <article className="min-h-screen px-4 md:px-0">
-        <Navbar />
-        <div className="max-w-3xl mx-auto">
+      <article className="flex items-center justify-center min-h-screen px-4 md:px-0">
+        {/* <Navbar /> */}
+        <div className="max-w-xl mx-auto">
           <div className="grid gap-3">{children}</div>
         </div>
       </article>
-      <Footer />
     </>
   );
 };
