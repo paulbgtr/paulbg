5c2027a81b04e7229e62355177e0ea3f61c1f478
paulbg <paulbg@hey.com>
2024-11-18 17:33:03 +0200

    chore: remove navbar, make responsive

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,9 @@
 import { ReactNode } from "react";
 
-import { Navbar } from "./Navbar";
-
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <>
-      <article className="flex items-center justify-center min-h-screen px-4 md:px-0">
-        {/* <Navbar /> */}
-        <div className="max-w-xl mx-auto">
-          <div className="grid gap-3">{children}</div>
-        </div>
-      </article>
-    </>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-xl px-4">{children}</div>
+    </div>
   );
 };
