bcecaeace288eec9d8cc0f099213d92621a362b4
paulbg <paulbg@hey.com>
2024-11-18 17:20:12 +0200

    chore: simplify header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,22 @@
-import { ReactNode } from "react";
-import Image from "next/image";
-import pfp from "/public/pfp.jpeg";
+import { MdOutlinePlace } from "react-icons/md";
 
-export const Header = ({
-  title,
-  description,
-  image,
-  children,
-}: {
-  title: string;
-  description: string;
-  image?: boolean;
-  children?: ReactNode;
-}) => {
+export const Header = () => {
   return (
     <section className="flex gap-8">
-      {/* {image && (
-        <div className="avatar">
-          <div className="w-24 h-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <Image src={pfp} alt={title} />
-          </div>
-        </div>
-      )} */}
-      <div className="space-y-1">
-        <h1 className="text-4xl font-black">{title}</h1>
-        <p className="text-sm italic">{description}</p>
-        {children}
+      <div className="space-y-2">
+        <h1 className="text-3xl font-black">Paul Bogatyr</h1>
+
+        <span className="flex items-center gap-1 text-gray-400">
+          <MdOutlinePlace size="20" />
+          <p className="text-sm">Tallinn, Estonia</p>
+        </span>
+
+        <p>
+          I&apos;m a full-stack developer who loves turning ideas into
+          functional web applications. I&apos;m driven by the challenge of
+          solving real problems through clean, creative code that makes a
+          genuine difference.
+        </p>
       </div>
     </section>
   );
