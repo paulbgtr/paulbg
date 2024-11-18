3c0337d448b6424e1b62206bef9abb559de90233
paulbg <paulbg@hey.com>
2024-11-18 17:20:32 +0200

    chore: make connect section more minimalist
    - remove icons
    - add smooth hover effect

--- src/components/pageRelated/index/Connect.tsx
+++ src/components/pageRelated/index/Connect.tsx
@@ -1,25 +1,31 @@
+import { Heading } from "@/components/Heading";
 import Link from "next/link";
 
-import { FaGithub, FaEnvelope, FaLinkedin, FaThreads } from "react-icons/fa6";
+interface LinkItemProps {
+  href: string;
+  label: string;
+}
+
+const LinkItem = ({ href, label }: LinkItemProps) => (
+  <Link
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="relative block transition-colors hover:text-gray-600 dark:hover:text-gray-300 after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:bg-current after:transition-all after:duration-300 hover:after:w-0"
+  >
+    {label}
+  </Link>
+);
 
 export const Connect = () => {
   return (
-    <div className="flex gap-3 mt-2">
-      <Link className="btn btn-ghost" href="https://github.com/paulbgtr">
-        <FaGithub size="22" />
-      </Link>
-      <Link
-        className="btn btn-ghost"
-        href="https://www.linkedin.com/in/paulbg0"
-      >
-        <FaLinkedin size="22" />
-      </Link>
-      <Link className="btn btn-ghost" href="/contact">
-        <FaEnvelope size="22" />
-      </Link>
-      <Link className="btn btn-ghost" href="https://www.threads.net/@paulbgtr">
-        <FaThreads size="22" />
-      </Link>
-    </div>
+    <section className="space-y-3">
+      <Heading text="Connect" />
+      <div className="flex gap-6">
+        <LinkItem href="https://github.com/paulbgtr" label="GitHub" />
+        <LinkItem href="https://linkedin.com/in/paulbg0" label="LinkedIn" />
+        <LinkItem href="mailto:paul@appstatis.com" label="Email" />
+      </div>
+    </section>
   );
 };
