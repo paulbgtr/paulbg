f385ae5dd63230dfbefde9f1f449ff2d7fd777f2
paulbg <paulbg@hey.com>
2024-11-18 17:19:28 +0200

    feat: use geist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import "@/styles/tailwind.css";
 import type { AppProps } from "next/app";
+import { GeistSans } from "geist/font/sans";
 
 import { Layout } from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <main className={GeistSans.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
   );
 }
