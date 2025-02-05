ac4c42466db04e7bf724ccfee600b18a0a2b33dd
paulbg <paulbg@hey.com>
2025-02-05 15:45:15 +0200

    chore: use space_mono

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import "@/styles/tailwind.css";
 import type { AppProps } from "next/app";
-import { GeistSans } from "geist/font/sans";
+import { Space_Mono } from "next/font/google";
+
+const spaceMono = Space_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-space-mono",
+});
 
 import { Layout } from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={GeistSans.className}>
+    <main className={spaceMono.className}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
