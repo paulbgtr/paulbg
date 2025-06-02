c075e24de68237ddbde7e5e870c21169ebe8b2ed
paulbg <paulbg@hey.com>
2025-06-02 23:11:21 +0300

    style(components/Layout.tsx): add media query to layout styles for responsive design feat(components/Layout.tsx): add md class to layout styles to enable responsive design

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,7 @@ export const Layout = ({ children }: { children: ReactNode }) => {
         <div className="absolute inset-0 bg-black opacity-30"></div>
 
         {/* Main content container */}
-        <div className="relative z-10 w-full max-w-xl p-8 border-[0.5px] border-gray-700 shadow-2xl rounded-xl backdrop-blur-lg">
+        <div className="relative z-10 w-full max-w-xl p-8 md:border-[0.5px] border-gray-700 shadow-2xl rounded-xl backdrop-blur-lg">
           {children}
         </div>
       </div>
